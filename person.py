ea026d68a427699d411ae4c32f3bc268f36b897a
Andela-Didacus <didacus.abongo@andela.com>
2017-03-17 10:58:12 +0300

    [feature] add function to filter edge cases

--- person.py
+++ person.py
@@ -30,3 +30,36 @@ class Person:
 
         except:
             pass
+
+def create_person(first_name, last_name, role, accomodation_status):
+    if (type(first_name) is not str or type(last_name) is not str) or (type(role) is not str) or (type(accomodation_status) is not str):
+            print colored("INVALID INPUT! ONLY STRINGS ALLOWED!!", "red")
+            return "INVALID INPUT!! TYPE ERROR!!"
+    else:  
+        if len(first_name) == 0 or len(last_name) == 0:
+            print colored("INVALID INPUT! BOTH NAMES MUST BE PROVIDED", "red")
+            return "INVALID NAME INPUT!!"
+        else:
+            if accomodation_status not in ["y","Y","N","n"]:
+                print colored("INVALID INPUT! ACCOMODATION STATUS CAN ONLY BE \'Y\' OR \'N\'", "red")
+                return "INVALID ACCOMODATION STATUS INPUT"
+            else:
+                try:
+                    role = role.upper()
+                    if role not in ["STAFF","FELLOW"]:
+                        print colored("INVALID INPUT! STAFF CAN ONLY BE STAFF OR FELLOW", "red")
+                        return "INVALID ROLE INPUT!!"
+                    else:
+                        first_name = first_name.upper()
+                        last_name = last_name.upper()
+                        accomodation_status = accomodation_status.upper()
+                        first_name = Person(first_name, last_name, role, accomodation_status)
+                        first_name.add_person()
+                        if role == "STAFF":
+                            Person.staff.append(first_name)
+                        elif role == "FELLOW":
+                            Person.fellows.append(first_name)
+
+                except:
+                    print colored("NAMES, ROLES OR ACCOMODATION STATUS CANNOT BE A NUMBER", "red")
+                    return "INVALID NUMBER INPUT"
