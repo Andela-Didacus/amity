c777bca62cf6e8704878e97dbef390b85a3c9070
Andela-Didacus <didacus.abongo@andela.com>
2017-03-26 16:16:04 +0300

    [FEATURE] add function to create rooms

--- room.py
+++ room.py
@@ -1,7 +1,13 @@
+import sys
+import datetime
+import time
+from termcolor import colored
+
+from amity import Amity
+
+
 class Rooms(object):
-    offices = []
-    living_spaces = []
-    rooms = [offices, living_spaces]
+    pass
 
 
 class Office(Rooms):
@@ -20,4 +26,48 @@ class Living_space(Rooms):
         self.num_of_occupants = 0
         self.max_number = 4
         
-        
+def create_room(room_name, room_type):
+    if type(room_name) != str or type(room_type) != str or (room_name.isalpha() == False):
+        print colored("\nNOTICE!! INVALID NAME INPUT, NAME CANNOT BE NUMBER\n", "red")
+        return "INVALID input!!"
+
+    else:
+        room_name = room_name.upper()
+        room_type = room_type.upper()
+        if room_type == "LIVING_SPACE":
+            room_type = "LIVING SPACE"
+        else:
+            pass
+        if room_name in Amity.offices or room_name in Amity.living_spaces:
+            print colored("\nROOM NAME ALREADY EXISTS!! USE ANOTHER NAME\n", "red")
+            return "ROOM EXISTS!"
+        else:
+            day = time.time()
+            timestamp = str(datetime.datetime.fromtimestamp(day).strftime("%y-%m-%d"))
+            if room_type == "OFFICE":
+                room_name = Office(room_name, room_type, timestamp)
+                Amity.offices.append(room_name.room_name)
+                Amity.office_details.append(room_name)
+                printer_room(room_name.room_name, room_name.room_type, room_name.timestamp)
+                return "OFFICE SUCCESSFULY CREATED SUCCESSFULLY CREATED!!"
+            elif room_type == "LIVING SPACE":
+                room_name = Living_space(room_name, room_type, timestamp)
+                Amity.living_spaces.append(room_name.room_name)
+                Amity.living_space_details.append(room_name)
+                printer_room(room_name.room_name, room_name.room_type, room_name.timestamp)
+                return "LIVING SPACE SUCCESSFULY CREATED SUCCESSFULLY CREATED!!"
+            else:
+                print colored("\nINVALID INPUT!! ROOM CAN ONLY BE OFFICE OR LIVING SPACE\n", "red")
+                return "INVALID ROOM TYPE INPUT!!"
+            
+def printer_room(room_name, room_type, timestamp):
+    print colored("\n*--* AMITY *--*", "cyan")
+    print colored("     -----      ", "cyan")
+    print "ROOM_NAME: %s"%room_name
+    print "ROOM TYPE: %s"%room_type
+    print "DATE CREATED: %s"%timestamp
+    print colored("*--*--*--*--*--*--*--*\n", "cyan")
+
+create_room("dida", "office")
+print Amity.all_rooms_details
+print Amity.offices
